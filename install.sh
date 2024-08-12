171fa0b814e08954175a8d6d77079b12221c23e3
Miguel Amador <miguesdev@gmail.com>
2024-08-12 15:27:04 -0500

    fix: the shell session does not reload after installation

--- install.sh
+++ install.sh
@@ -22,13 +22,13 @@ curl -s -o $HOME/.zsh-utils https://migueweb.github.io/zsh-utils/zshutils
 if [[ $SHELL == */bash ]]; then # if the shell is bash
   success "installing zsh-utils..."
   echo -e "\nsource $HOME/.zsh-utils" >> $HOME/.bashrc
-  source $HOME/.bashrc
+  bash
   success "zsh-utils installed successfully."
 
 elif [[ $SHELL == */zsh ]]; then # if the shell is zsh
   success "installing zsh-utils..."
   echo -e "\nsource $HOME/.zsh-utils" >> $HOME/.zshrc
-  source $HOME/.zshrc
+  zsh
   success "zsh-utils installed successfully."
 else
     error "you need to use Bash or Zsh to install zsh-utils."
